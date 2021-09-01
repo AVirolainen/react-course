b599903fe544b5c0823ee226bfe19444d1f76862
Rudenko Stanislav <rudenkostas2@gmail.com>
2021-09-01 19:13:50 +0300

    init project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import "./Header.css"
+
+const Header=()=>{
+    return(
+        <div className="HeaderBody">
+            <img className="logoImage" src="https://thumbs.dreamstime.com/b/orange-slice-icon-orange-peace-vector-illustration-isolated-white-tasty-sweet-fruit-symbol-orange-slice-icon-orange-peace-193309201.jpg" />
+        </div>
+    )
+}
+
+
+export default Header
+
+
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import "./Navbar.css"
+
+const Navbar=()=>{
+    return(
+        <div className="NavbarBody">
+            <div className="avatarImage"></div>
+            <div className="usernameText">Valerii Zhmyshenko</div>
+            <div className="">
+                <a></a>
+                <a></a>
+                <a></a>
+                <a></a>
+                <a></a>
+            </div>
+        </div>
+    )
+}
+
+
+export default Navbar
+
+
+
+
